48a6d4d1bac23682ca4fc3917f1de44c6018f289
Kuladeepjayker <jayker.kuladeep@gmail.com>
2024-08-25 13:05:43 +0530

    added Responsible designs

--- src/components/starbox.jsx
+++ src/components/starbox.jsx
@@ -19,20 +19,20 @@ function Starbox() {
       {isMobile ? (
         <div className='starBox1'>
           <div className='starBox-card1'>
-            <Card.Img className="starBox-img1 star-left1" src="Assets/teenyicons_star-solid.svg" alt="starBox image" />
+            <Card.Img className="starBox-img1 star-left1" src="/Assets/teenyicons_star-solid.svg" alt="starBox image" />
             <div className='starBox-text'>
               <h2>Reel Deal Monthly Movies</h2>
               <h5 className='starBox-tag1'>Your flexible monthly menbership to the latest movies</h5>
             </div>
-            <Card.Img className="starBox-img1 star-left1" src="Assets/teenyicons_star-solid-1.svg" alt="starBox image" />
+            <Card.Img className="starBox-img1 star-left1" src="/Assets/teenyicons_star-solid-1.svg" alt="starBox image" />
           </div>
         </div>
         ) : (
         <div className='starBox'>
           <div className='starBox-card'>
-            <Card.Img className="starBox-img star-left" src="Assets/teenyicons_star-solid.svg" alt="starBox image" />
-            <Card.Img className="starBox-img star-center" src="Assets/teenyicons_star-solid-2.svg" alt="starBox image" />
-            <Card.Img className="starBox-img star-left" src="Assets/teenyicons_star-solid-1.svg" alt="starBox image" />
+            <Card.Img className="starBox-img star-left" src="/Assets/teenyicons_star-solid.svg" alt="starBox image" />
+            <Card.Img className="starBox-img star-center" src="/Assets/teenyicons_star-solid-2.svg" alt="starBox image" />
+            <Card.Img className="starBox-img star-left" src="/Assets/teenyicons_star-solid-1.svg" alt="starBox image" />
           </div>
           <div className='starBox-text'>
             <h2>Reel Deal Monthly Movies</h2>
